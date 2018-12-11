c7ea3034f9acd0fba110c716f6f28d458b340eb8
Delyan Iliev <diliev1996@gmail.com>
2018-12-11 20:40:51 +0100

    .

--- AdsPicker/Program.cs
+++ AdsPicker/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Net.Http;
 using System.Drawing;
-using System.Xml;
 using System.IO;
-using System.Text;
 using Newtonsoft.Json;
 using System.Threading;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace AdsPicker
 {
@@ -14,29 +14,35 @@ namespace AdsPicker
         public string ImageData { get; set; }
     }
 
-    internal class Program
+    public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("settings.json", optional: true, reloadOnChange: true);
+            var configuration = builder.Build();
+
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri(configuration["addServiceAddress"]),
+            };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
             while (true)
             {
                 try
                 {
-                    HttpClient client = new HttpClient
-                    {
-                        BaseAddress = new Uri("http://adservice.ws.dm.sof60.dk/"),
-                    };
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-                    var result = client.GetStringAsync("api/ad").GetAwaiter().GetResult();
+                    var resultByte = client.GetByteArrayAsync(configuration["addServiceAPIEndpoint"]).GetAwaiter().GetResult();
+                    string result = System.Text.Encoding.UTF8.GetString(resultByte);
                     var root = JsonConvert.DeserializeObject<RootObject>(result);
                     using (var ms = new MemoryStream(Convert.FromBase64String(root.ImageData)))
                     {
                         using (var bitmap = new Bitmap(ms))
                         {
                             Console.WriteLine("Success");
-                            //send string to server
+                            client.PostAsync(configuration["parkingAdsAddress"] + configuration["parkingAdsAPIEndpoint"],
+                                new ByteArrayContent(resultByte));
                         }
                     }
                 }
---
+++ ParkingAdsAPI/Controllers/AdsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParkingAdsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdsController : ControllerBase
+    {
+        public static string imageStr;
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void Post(string base64str)
+        {
+            if (!string.IsNullOrEmpty(base64str))
+            {
+                imageStr = base64str;
+            }
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return imageStr;
+        }
+    }
+}
