900c7e02a18dee89673b11c0b0cd489d17d9fc04
Delyan Iliev <diliev1996@gmail.com>
2019-01-15 20:51:20 +0100

    .

--- AdsPicker/Program.cs
+++ AdsPicker/Program.cs
@@ -18,7 +18,6 @@ namespace AdsPicker
     {
         public static void Main(string[] args)
         {
-            var path = Directory.GetCurrentDirectory();
             var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("settings.json", optional: true, reloadOnChange: true);
